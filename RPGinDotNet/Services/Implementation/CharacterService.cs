e1fe0b77a8766a9df124b1dda44f46a2864be529
Ghiza <matei.ghizasan@itperspectives.ro>
2022-11-10 00:05:45 +0200

    nothing

--- RPGinDotNet/Services/Implementation/CharacterService.cs
+++ RPGinDotNet/Services/Implementation/CharacterService.cs
@@ -12,7 +12,7 @@ namespace RPGinDotNet.Services.Implementation
         private static Character defaultCharacter = new() { Name = "default", Id = 0 };
         private static List<Character> characters = new()
         {
-            new Character(){Name = "cineva",Id = 2, HitPoints = 100,Class = RpgClass.Mage},
+            new Character(){Name = "cineva",Id = 2, HitPoints = 100,Defence = 20,Class = RpgClass.Mage},
             new Character(){Name = "Ghiza",Id = 1, Intelligence = 69,Class = RpgClass.Knight}
         };
 
